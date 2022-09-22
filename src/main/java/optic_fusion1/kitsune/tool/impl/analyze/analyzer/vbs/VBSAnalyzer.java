56ab7650d016c6f62f33d88d4839d2ac99112ddd
OpticFusion1 <37254722+OpticFusion1@users.noreply.github.com>
2022-09-22 17:14:41 -0400

    Version 1.4.3 VBSAnalyzer is actually useful now

--- src/main/java/optic_fusion1/kitsune/tool/impl/analyze/analyzer/vbs/VBSAnalyzer.java
+++ src/main/java/optic_fusion1/kitsune/tool/impl/analyze/analyzer/vbs/VBSAnalyzer.java
@@ -3,6 +3,10 @@ package optic_fusion1.kitsune.tool.impl.analyze.analyzer.vbs;
 import java.io.File;
 import static optic_fusion1.kitsune.Kitsune.LOGGER;
 import optic_fusion1.kitsune.parser.vbs.Comment;
+import optic_fusion1.kitsune.parser.vbs.ConstStatement;
+import optic_fusion1.kitsune.parser.vbs.MsgBoxStatement;
+import optic_fusion1.kitsune.parser.vbs.Parameter;
+import optic_fusion1.kitsune.parser.vbs.SetStatement;
 import optic_fusion1.kitsune.parser.vbs.Statement;
 import optic_fusion1.kitsune.parser.vbs.VBSParser;
 import optic_fusion1.kitsune.tool.impl.analyze.analyzer.Analyzer;
@@ -14,8 +18,21 @@ public class VBSAnalyzer extends Analyzer {
         VBSParser parser = new VBSParser(input);
         parser.parse();
         for (Statement statement : parser.getParsedStatements()) {
+            if (statement instanceof ConstStatement stmnt) {
+                LOGGER.info("Const: " + stmnt.getName() + " Value: " + stmnt.getValue());
+                continue;
+            }
+            if (statement instanceof MsgBoxStatement stmnt) {
+                LOGGER.info("MsgBox: " + stmnt.getMessage());
+                continue;
+            }
+            if (statement instanceof SetStatement stmnt) {
+                LOGGER.info("Variable: " + stmnt.getVariableName() + " Value: " + stmnt.getVariableValue().getText());
+                continue;
+            }
             if (statement instanceof Comment comment) {
-                LOGGER.info(comment.value());
+                LOGGER.info("Comment: " + comment.getText());
+                continue;
             }
         }
     }
